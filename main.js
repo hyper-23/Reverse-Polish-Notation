7204bc616566c41465dc8a4a9bae86e904d00f55
vagrant <vagrant@vm-test>
2020-06-14 00:58:33 +0900

    init

--- main.js
+++ main.js
@@ -4,31 +4,31 @@
  */
 var rpn = function(S) {
     S = S.split(" ")
-    var que = []
+    var que = [];
     var tmp = 0;
     for(i=0; i<S.length; i++){
         if(S[i].match(/[0-9]/)) {
             que.push(Number(S[i]));
         }else if(S[i]=="+") {
-            tmp = que[que.length-2]+que[que.length-1]
+            tmp = que[que.length-2]+que[que.length-1];
             que.splice(que.length-2, 2);
             que.push(tmp);
         }else if(S[i]=="-") {
-            tmp = que[que.length-2]-que[que.length-1]
+            tmp = que[que.length-2]-que[que.length-1];
             que.splice(que.length-2, 2);
             que.push(tmp);
         }else if(S[i]=="*") {
-            tmp = que[que.length-2]*que[que.length-1]
+            tmp = que[que.length-2]*que[que.length-1];
             que.splice(que.length-2, 2);
             que.push(tmp);
         }else if(S[i]=="/") {
-            tmp = que[que.length-2]/que[que.length-1]
+            tmp = que[que.length-2]/que[que.length-1];
             que.splice(que.length-2, 2);
             que.push(tmp);
         }
-        console.log("S:"+S+" que:"+que)
+        console.log("S:"+S+" que:"+que);
     }
-    return Number(que.join(""))
+    return Number(que.join(""));
 };
 
-console.log(rpn("4 2 + 5 3 + *"))
+console.log(rpn("4 2 + 5 3 + *"));
